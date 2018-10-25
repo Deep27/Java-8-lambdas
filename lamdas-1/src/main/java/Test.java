7d400f2ded5c5caed20854608d1f4b1cb424216d
pendalf <r.sotnichenko@yandex.ru>
2018-10-25 20:30:39 +0300

    Add readme to l1.

---
+++ lamdas-1/src/main/java/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+
+//        failure example (name is not final or effectively final)
+//        String name = getUserName();
+//        name = formatUserName(name);
+//        button.addActionListener(event -> System.out.println("hi " + name));
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
